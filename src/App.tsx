04708028a18803c165b92059e163e7e70f5d961c
joe <94115299+joe-dev-public@users.noreply.github.com>
2024-02-13 11:00:19 +0000

    Hello World request of local FastAPI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+
+// Todo: add types where necessary/recommended
 
 function App() {
+  const [message, setMessage] = useState("Default text");
+
+  useEffect(() => {
+    const url = "http://127.0.0.1:8000/";
+
+    fetch(url)
+      .then((response) => {
+        // console.log("response, ", response);
+
+        return response.json();
+
+        // Todo: work out why the false delay doesn't work here (probably
+        // something to do with returning the setTimeout rather than the
+        // resolved promise?)
+        // return setTimeout(() => response.json(), 1000);
+      })
+      .then((response) => {
+        // console.log("response, ", response);
+
+        // setMessage(response.message);
+
+        // False delay:
+        return setTimeout(() => setMessage(response.message), 1500);
+      });
+  });
+
   return (
     <>
-      <h1>Hello world</h1>
+      <h1>{message}</h1>
     </>
   );
 }
